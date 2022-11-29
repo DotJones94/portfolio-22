5261201a82ab0d5d68a9bdf9b1bdee29589e95b6
kellydotjones <kellydotjones2@gmail.com>
2022-11-29 23:24:44 +0000

    Hover skills

--- src/components/elements/skill-block.vue
+++ src/components/elements/skill-block.vue
@@ -1,34 +1,25 @@
 <script>
+import skillItem from './skill-item.vue'
 export default {
+  components: { skillItem },
     props: {
-        skill : Object
+        skill : Object,
+    },
+    data(){
+        return{
+            hover : false,
+        }
     }
 }
 </script>
 <template>
-    <div class="skill-block">
+    <div class="skill-block"
+     @mouseover="hover = true" @mouseleave="hover = false"
+     :class="{'active' : hover}">
         <img :scr="skill.image" class="" />
         <div class="heading__six">{{skill.name}}</div>
         <div class="skill-block__amounts">
-            <div v-for="skill in skill.skills" :key="skill.id" class="amount"> 
-                <div class="amount__percentage--text">{{skill.percentage}}</div>
-                <div class="amount__percentage">
-                    <svg viewBox="0 0 4.5 4.5" xmlns="http://www.w3.org/2000/svg">
-                        <!-- Lower circle -->
-                        <circle cx="2.25" cy="2.25" r="2" 
-                        fill="transparent" stroke="rgba(255,255,255,0.3)"
-                        stroke-width="0.35" />
-                        <!-- Actual circle -->  
-                        <circle cx="2.25" cy="2.25" r="2" 
-                        fill="transparent" stroke="rgba(255,255,255,1)" 
-                        stroke-width="0.5" 
-                        :stroke-dasharray="skill.strokeDasharray" 
-                        stroke-linecap="round" transform="rotate(-90 2.25 2.25)" />
-                    </svg>
-                </div>
-                <div class="amount__name heading__five">{{skill.name}}</div>
-            </div>
+            <skill-item v-for="skill in skill.skills" :key="skill.id" :skillItem="skill" />
         </div>
-        
     </div>
 </template>
