de3acfa213914cd92e1872719078104de3082417
kellydotjones <kellydotjones2@gmail.com>
2022-12-03 13:25:15 +0000

    Front page animations

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -6,82 +6,94 @@ import Skills from './page-sections/skills.vue';
 import Qualifications  from './page-sections/qualifications.vue';
 import ContactMe  from './page-sections/contact-us.vue';
 import { mapState } from 'vuex';
+import Bee from './elements/bee.vue';
 </script>
 
 <template>
-    <div >
+    <div>
+        <!-- Laoding -->
+        <div class="circle__wrapper full-layer">
+            <div class="circle circle__outer" :class="{'circle--loaded': loading}"></div>
+            <div class="circle circle__inner" :class="{'circle--loaded': loading}"></div>
+            <div class="circle circle__inner-most" :class="{'circle--loaded': loading}"></div>
+        </div>
+
         <!-- Bees -->
-        <div class="bees full-layer">
+        <div class="bees full-layer" v-if="loading">
             <div v-for="index in 4" :key="index" class="landing-page__bees--single">
-                <img src="../assets/images/bee_template.png" alt="bee template" />
+               <Bee class="bee"/>
             </div>
         </div>
 
-        <SideNavigation/>
+        <SideNavigation v-if="loading"/>
         <div class="pages">
-            <LandingPage id="home"/>
-            <Experience id="experience"/>
-            <skills id="skills" />
-            <Qualifications id="qualifications"/>
-            <ContactMe id="contact-me"/>
+            <LandingPage id="home" v-if="loading"/>
+            <Experience id="experience" v-if="loading"/>
+            <skills id="skills" v-if="loading"/>
+            <Qualifications id="qualifications" v-if="loading"/>
+            <ContactMe id="contact-me" v-if="loading"/>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    created () {
-        window.addEventListener('scroll', this.navigationChange);
+    created() {
+        window.addEventListener("scroll", this.navigationChange);
     },
-    destroyed () {
-        window.removeEventListener('scroll', this.navigationChange);
+    destroyed() {
+        window.removeEventListener("scroll", this.navigationChange);
     },
-    computed:{
+    mounted() {
+        console.log("here");
+        this.loading = true;
+    },
+    computed: {
         ...mapState({
             activeSection: "activeSection"
         })
     },
     watch: {
-        activeSection : function(newSectionName){
+        activeSection: function (newSectionName) {
             const element = document.getElementById(newSectionName);
-
-            if(element) element.scrollIntoView({behavior: 'smooth', block: 'center'});
+            if (element)
+                element.scrollIntoView({ behavior: "smooth", block: "center" });
         }
     },
+    data() {
+        return {
+            loading: false
+        };
+    },
     methods: {
-        navigationChange(){
-            let home = Math.ceil(document.getElementById('home').getBoundingClientRect().top);
-            let experience = Math.ceil(document.getElementById('experience').getBoundingClientRect().top);
-            let skills = Math.ceil(document.getElementById('skills').getBoundingClientRect().top);
-            let qualifications = Math.ceil(document.getElementById('qualifications').getBoundingClientRect().top);
-            let contactMe = Math.ceil(document.getElementById('contact-me').getBoundingClientRect().top);
-
-
-            if(home >= 0 && home < 50) 
-                this.$store.commit('changeSection', {
-                    newSectionName: 'home'
-                })
-
-            if(experience > 0 && experience < 50) 
-                this.$store.commit('changeSection', {
-                    newSectionName: 'experience'
-                })
-
-            if(skills > 0 && skills < 50) 
-                this.$store.commit('changeSection', {
-                    newSectionName: 'skills'
-                })
-            
-            if(qualifications > 0 && qualifications < 50) 
-                this.$store.commit('changeSection', {
-                    newSectionName: 'qualifications'
-                })
-            
-            if(contactMe > 0 && contactMe < 50) 
-                this.$store.commit('changeSection', {
-                    newSectionName: 'contact-me'
-                })
+        navigationChange() {
+            let home = Math.ceil(document.getElementById("home").getBoundingClientRect().top);
+            let experience = Math.ceil(document.getElementById("experience").getBoundingClientRect().top);
+            let skills = Math.ceil(document.getElementById("skills").getBoundingClientRect().top);
+            let qualifications = Math.ceil(document.getElementById("qualifications").getBoundingClientRect().top);
+            let contactMe = Math.ceil(document.getElementById("contact-me").getBoundingClientRect().top);
+            if (home >= 0 && home < 50)
+                this.$store.commit("changeSection", {
+                    newSectionName: "home"
+                });
+            if (experience > 0 && experience < 50)
+                this.$store.commit("changeSection", {
+                    newSectionName: "experience"
+                });
+            if (skills > 0 && skills < 50)
+                this.$store.commit("changeSection", {
+                    newSectionName: "skills"
+                });
+            if (qualifications > 0 && qualifications < 50)
+                this.$store.commit("changeSection", {
+                    newSectionName: "qualifications"
+                });
+            if (contactMe > 0 && contactMe < 50)
+                this.$store.commit("changeSection", {
+                    newSectionName: "contact-me"
+                });
         },
-    }
+    },
+    components: { Bee }
 }
 </script>
--- src/components/page-sections/landing-page.vue
+++ src/components/page-sections/landing-page.vue
@@ -6,23 +6,31 @@
         components: {
             Send, palm
         },
+        data(){
+            return{
+                typography : 'heading--loading'
+            }
+        }
     }
 </script>
 
 <template>
     <div class="full_page landing-page">
         <!-- Content -->
-        <div class="full-layer">
+        <div class="full-layer full-layer--background">
             <div class="landing-page__content">
                 <!-- Heading of my name -->
-                <h1 class="heading__main landing-page__heading">
-                    Kelly Dot Jones
+                <h1 class="heading__main landing-page__heading"
+                    :class="'landing-page__' + typography">
+                    <div>Kelly</div>
+                    <div>Dot</div>  
+                    <div>Jones</div>
                 </h1>
 
                 <!-- Welcome note -->
-                <div class="landing-page__welcome-note top__navigation">
+                <div class="landing-page__welcome-note top__navigation" >
                     <div class="line_breaker"></div>
-                    <h2 class="heading__two">Welcome to my portfolio</h2>
+                    <div><h2 class="heading__two">Welcome to my portfolio</h2></div>
                     <div class="description">Something short sweet and a little funny, like a quote or something that shows a little bit more about me</div>
                     <div class="btn btn--main ">
                         <div class="btn__circle"></div>
@@ -34,19 +42,19 @@
                 </div>
             </div>           
         </div>
-        
-
+    
         <!-- Background circle -->
         <div class="full-layer full-layer--background">
             <div class="landing-page__background landing-page__background--circle"></div>
         </div>
 
+        <!-- Plant Shaddow -->
         <div class="full-layer full-layer--background">
-            <div class="circle-shaddow"></div>
+            <div class="landing-page__background"><div class="circle-shaddow"></div></div>
         </div>
 
         <!-- Image of plant -->
-        <div class="full-layer full-layer--background">
+        <div class="full-layer full-layer--background full-layer--background-plant" >
             <palm class="landing-page__background landing-page__background--plant"/>
            
         </div>
